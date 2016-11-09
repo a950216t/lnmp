16410614a1f5370410f1c1b442f9fff1db897a89
a950216t <a950216t@gmail.com>
2016-11-09 19:34:48 +0800

    update

--- vhost.sh
+++ vhost.sh
@@ -347,7 +347,7 @@ Input_Add_domain() {
     [ -z "$(grep 'Listen 443' ${apache_install_dir}/conf/httpd.conf)" ] && sed -i "s@Listen 80@&\nListen 443@" ${apache_install_dir}/conf/httpd.conf
     [ -z "$(grep 'ServerName 0.0.0.0:443' ${apache_install_dir}/conf/httpd.conf)" ] && sed -i "s@ServerName 0.0.0.0:80@&\nServerName 0.0.0.0:443@" ${apache_install_dir}/conf/httpd.conf
   else
-    Nginx_conf=$(echo -e "listen 80;\nlisten [::]:80;")
+    Nginx_conf=$(echo -e "listen 80;\n  listen [::]:80;")
   fi
 
   while :; do echo
