4fa636c1085cc2b5e4a3ecce73a3378ebebab83a
a950216t <a950216t@gmail.com>
2025-06-14 10:50:27 +0800

    改成 LNMP 架構，去除 Apache

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 <div align="center">
-    <a href="https://lamp.sh/" target="_blank">
-        <img alt="LAMP" src="https://github.com/teddysun/lamp/blob/master/conf/lamp.png">
+    <a href="https://lnmp.999199.xyz/" target="_blank">
+        <img alt="LNMP" src="https://github.com/a950216t/lnmp/blob/main/conf/lnmp.png">
     </a>
 </div>
 
 ## Description
 
-LAMP (Linux + Apache + MariaDB + PHP) is a powerful bash script for the installation of Apache + MariaDB + PHP and so on.
+LNMP (Linux + Nginx + MariaDB + PHP) is a powerful bash script for the installation of Nginx + MariaDB + PHP and so on.
 
-You can install Apache + MariaDB + PHP in a smaller memory VPS by `apt-get` command, Just need to input numbers to choose what you want to install before installation.
+You can install Nginx + MariaDB + PHP in a smaller memory VPS by `apt-get` command, Just need to input numbers to choose what you want to install before installation.
 
 And all things will be done in a few minutes.
 
@@ -21,7 +21,7 @@ And all things will be done in a few minutes.
 - [Uninstall](#uninstall)
 - [Default Location](#default-location)
 - [Process Management](#process-management)
-- [lamp command](#lamp-command)
+- [lnmp command](#lnmp-command)
 - [Bugs & Issues](#bugs--issues)
 - [License](#license)
 
@@ -43,7 +43,7 @@ And all things will be done in a few minutes.
 
 ## Supported Software
 
-- Apache 2.4  ※ Apache packages provided by Official Repository
+- Nginx Latest Stable  ※ Nginx packages provided by [deb.sury.org](https://deb.sury.org/)
 - MariaDB 10.11, 11.4  ※ MariaDB packages provided by [MariaDB Repository](https://dlm.mariadb.com/browse/mariadb_server/)
 - PHP 7.4, 8.0, 8.1, 8.2, 8.3, 8.4  ※ PHP packages provided by [deb.sury.org](https://deb.sury.org/)
 
@@ -56,16 +56,16 @@ And all things will be done in a few minutes.
 
 ```bash
 apt-get -y install wget git
-git clone -b deb https://github.com/teddysun/lamp.git
-cd lamp
+git clone -b deb https://github.com/a950216t/lnmp.git
+cd lnmp
 chmod 755 *.sh
-./lamp.sh 2>&1 | tee lamp.log
+./lnmp.sh 2>&1 | tee lnmp.log
 ```
 
 ## Upgrade
 
 ```bash
-apt-get install --only-upgrade -y apache2
+apt-get install --only-upgrade -y nginx
 apt-get install --only-upgrade -y mariadb-*
 # for example: php_ver=[7.4|8.0|8.1|8.2|8.3|8.4]
 php_ver="8.2"
@@ -75,7 +75,7 @@ apt-get install --only-upgrade -y php${php_ver}-*
 ## Uninstall
 
 ```bash
-apt-get remove -y apache2
+apt-get remove -y nginx
 apt-get remove -y mariadb-*
 # for example: php_ver=[7.4|8.0|8.1|8.2|8.3|8.4]
 php_ver="8.2"
@@ -84,11 +84,11 @@ apt-get remove -y php${php_ver}-*
 
 ## Default Location
 
-| Apache Location            | Path                                        |
+| Nginx Location            | Path                                         |
 |----------------------------|---------------------------------------------|
 | Web root location          | /data/www/default                           |
-| Main Configuration File    | /etc/apache2/apache2.conf                   |
-| Sites Configuration Folder | /etc/apache2/sites-enabled                  |
+| Main Configuration File    | /etc/nginx/nginx.conf                       |
+| Sites Configuration Folder | /etc/nginx/vhost                            |
 
 | MariaDB Location           | Path                                        |
 |----------------------------|---------------------------------------------|
@@ -104,34 +104,35 @@ apt-get remove -y php${php_ver}-*
 
 | Process     | Command                                                    |
 |-------------|------------------------------------------------------------|
-| Apache      | systemctl [start\|stop\|status\|restart] apache2           |
+| Nginx       | systemctl [start\|stop\|status\|restart] nginx             |
 | MariaDB     | systemctl [start\|stop\|status\|restart] mariadb           |
 | PHP         | systemctl [start\|stop\|status\|restart] php${php_ver}-fpm |
 
-## lamp Command
+## lnmp Command
 
 | Command          | Description                                           |
 |------------------|-------------------------------------------------------|
-| lamp start       | Start all of LAMP services                            |
-| lamp stop        | Stop all of LAMP services                             |
-| lamp restart     | Restart all of LAMP services                          |
-| lamp status      | Check all of LAMP services status                     |
-| lamp version     | Print all of LAMP software version                    |
-| lamp vhost add   | Create a new Apache virtual host                      |
-| lamp vhost list  | List all of Apache virtual hosts                      |
-| lamp vhost del   | Delete a Apache virtual host                          |
-| lamp db add      | Create a MariaDB database and a user with same name   |
-| lamp db list     | List all of MariaDB databases                         |
-| lamp db del      | Delete a MariaDB database and a user with same name   |
-| lamp db edit     | Update a MariaDB database username's password         |
+| lnmp start       | Start all of LNMP services                            |
+| lnmp stop        | Stop all of LNMP services                             |
+| lnmp restart     | Restart all of LNMP services                          |
+| lnmp status      | Check all of LNMP services status                     |
+| lnmp version     | Print all of LNMP software version                    |
+| lnmp vhost add   | Create a new Nginx virtual host                       |
+| lnmp vhost list  | List all of Nginx virtual hosts                       |
+| lnmp vhost del   | Delete a Nginx virtual host                           |
+| lnmp db add      | Create a MariaDB database and a user with same name   |
+| lnmp db list     | List all of MariaDB databases                         |
+| lnmp db del      | Delete a MariaDB database and a user with same name   |
+| lnmp db edit     | Update a MariaDB database username's password         |
 
 ## Bugs & Issues
 
-Please feel free to report any bugs or issues to us, email to: i@teddysun.com or [open issues](https://github.com/teddysun/lamp/issues) on Github.
+Please feel free to report any bugs or issues to us, email to: admin@999199.xyz or [open issues](https://github.com/a950216t/lnmp/issues) on Github.
 
 
 ## License
 
 Copyright (C) 2013 - 2025 [Teddysun](https://teddysun.com/)
+Copyright (C) 2025 [a950216t](https://999199.xyz/)
 
 Licensed under the [GPLv3](LICENSE) License.
--- lnmp.sh
+++ lnmp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This is a Shell script for VPS initialization and
-# LAMP (Linux + Apache + MariaDB + PHP) installation
+# LNMP (Linux + Nginx + MariaDB + PHP) installation
 #
 # Supported OS:
 # Debian 11
@@ -11,6 +11,7 @@
 # Ubuntu 24.04
 #
 # Copyright (C) 2013 - 2025 Teddysun <i@teddysun.com>
+# Copyright (C) 2025 a950216t <admin@999199.xyz>
 #
 trap _exit INT QUIT TERM
 
@@ -328,46 +329,38 @@ echo
 _info "Initialization completed"
 sleep 3
 clear
-_info "LAMP (Linux + Apache + MariaDB + PHP) installation start"
-_info "Apache installation start"
-_error_detect "apt-get install -y apache2 libapache2-mod-perl2 libapache2-mod-md"
-_error_detect "systemctl stop apache2"
-_error_detect "a2enmod ssl http2 brotli data unique_id vhost_alias echo expires info"
-_error_detect "a2enmod headers perl rewrite lua request md mime_magic remoteip userdir"
-_error_detect "a2enmod auth*"
-_error_detect "a2enmod cache*"
-_error_detect "a2enmod dav*"
-_error_detect "a2enmod proxy*"
-_error_detect "a2enmod session*"
-sed -i "s@^ServerTokens.*@ServerTokens Minimal@" "/etc/apache2/conf-available/security.conf"
-sed -i "s@^ServerSignature.*@ServerSignature Off@" "/etc/apache2/conf-available/security.conf"
-_info "Apache installation completed"
+_info "LNMP (Linux + Nginx + MariaDB + PHP) installation start"
+_info "Nginx installation start"
+# https://packages.sury.org/nginx/
+if check_sys debian; then
+    _error_detect "curl -sSLo /usr/share/keyrings/deb.sury.org-nginx.gpg https://packages.sury.org/nginx/apt.gpg"
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/deb.sury.org-nginx.gpg] https://packages.sury.org/nginx/ $(lsb_release -sc) main" >/etc/apt/sources.list.d/nginx.list
+fi
+# https://launchpad.net/~ondrej/+archive/ubuntu/nginx
+if check_sys ubuntu; then
+    _error_detect "add-apt-repository -y ppa:ondrej/nginx"
+fi
+_error_detect "apt-get update"
+_error_detect "apt-get -y upgrade"
+_error_detect "apt-get install -y nginx libnginx-mod-stream libnginx-mod-http-geoip libnginx-mod-http-image-filter libnginx-mod-http-perl libnginx-mod-http-xslt-filter libnginx-mod-mail libnginx-mod-stream-geoip"
+_error_detect "systemctl stop nginx"
+_info "Nginx installation completed"
 
-_info "Setting up Apache"
+_info "Setting up Nginx"
 _error_detect "mkdir -p /data/www/default"
 _error_detect "mkdir -p /data/wwwlog"
-_error_detect "mkdir -p /etc/apache2/ssl"
-_error_detect "cp -f ${cur_dir}/conf/apache2.conf /etc/apache2/apache2.conf"
-_error_detect "cp -f ${cur_dir}/conf/ssl.conf /etc/apache2/mods-available/"
+_error_detect "mkdir -p /etc/nginx/ssl"
+_error_detect "mkdir -p /etc/nginx/vhost"
+_error_detect "mv -f /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak"
+_error_detect "cp -f ${cur_dir}/conf/nginx.conf /etc/nginx/"
+_error_detect "cp -f ${cur_dir}/conf/proxy.conf /etc/nginx/"
+_error_detect "cp -f ${cur_dir}/conf/cloudflare.conf /etc/nginx/"
+_error_detect "cp -f -r ${cur_dir}/conf/rewrite /etc/nginx/"
 
 _error_detect "cp -f ${cur_dir}/conf/favicon.ico /data/www/default/"
 _error_detect "cp -f ${cur_dir}/conf/index.html /data/www/default/"
-_error_detect "cp -f ${cur_dir}/conf/lamp.png /data/www/default/"
-cat >/etc/apache2/sites-available/000-default.conf <<EOF
-<VirtualHost _default_:80>
-ServerName localhost
-DocumentRoot /data/www/default
-<Directory /data/www/default>
-    SetOutputFilter DEFLATE
-    Options FollowSymLinks
-    AllowOverride All
-    Order Deny,Allow
-    Allow from All
-    DirectoryIndex index.html index.htm index.php
-</Directory>
-</VirtualHost>
-EOF
-_info "Apache setup completed"
+_error_detect "cp -f ${cur_dir}/conf/lnmp.png /data/www/default/"
+_info "Nginx setup completed"
 
 _info "MariaDB installation start"
 _error_detect "wget -qO mariadb_repo_setup.sh https://dl.lamp.sh/files/mariadb_repo_setup.sh"
@@ -422,12 +415,13 @@ if check_sys ubuntu; then
     _error_detect "add-apt-repository -y ppa:ondrej/php"
 fi
 _error_detect "apt-get update"
+_error_detect "apt-get -y upgrade"
 _error_detect "apt-get install -y php-common php${php_ver}-common php${php_ver}-cli php${php_ver}-fpm php${php_ver}-opcache php${php_ver}-readline"
 _error_detect "apt-get install -y libphp${php_ver}-embed php${php_ver}-bcmath php${php_ver}-gd php${php_ver}-imap php${php_ver}-mysql php${php_ver}-dba php${php_ver}-mongodb php${php_ver}-sybase"
 _error_detect "apt-get install -y php${php_ver}-pgsql php${php_ver}-odbc php${php_ver}-enchant php${php_ver}-gmp php${php_ver}-intl php${php_ver}-ldap php${php_ver}-snmp php${php_ver}-soap"
 _error_detect "apt-get install -y php${php_ver}-mbstring php${php_ver}-curl php${php_ver}-pspell php${php_ver}-xml php${php_ver}-zip php${php_ver}-bz2 php${php_ver}-lz4 php${php_ver}-zstd"
 _error_detect "apt-get install -y php${php_ver}-tidy php${php_ver}-sqlite3 php${php_ver}-imagick php${php_ver}-grpc php${php_ver}-yaml php${php_ver}-uuid"
-_error_detect "mkdir -m770 /var/lib/php/{session,wsdlcache,opcache}"
+_error_detect "mkdir -p -m770 /var/lib/php/{sessions,wsdlcache,opcache}"
 _info "PHP installation completed"
 
 _info "Setting up PHP"
@@ -446,9 +440,9 @@ sed -i "s@^;php_admin_value\[error_log\].*@php_admin_value[error_log] = /var/log
 sed -i "s@^;php_admin_flag\[log_errors\].*@php_admin_flag[log_errors] = on@" "${php_conf}"
 cat >>"${php_conf}" <<EOF
 php_value[session.save_handler] = files
-php_value[session.save_path]    = /var/lib/php/session
+php_value[session.save_path]    = /var/lib/php/sessions
 php_value[soap.wsdl_cache_dir]  = /var/lib/php/wsdlcache
-php_value[opcache.file_cache]   = /var/lib/php/opcache
+;php_value[opcache.file_cache]   = /var/lib/php/opcache //會導致奇怪錯誤 bug 所以禁用
 EOF
 sed -i "s@^disable_functions.*@disable_functions = passthru,exec,shell_exec,system,chroot,chgrp,chown,proc_open,proc_get_status,ini_alter,ini_alter,ini_restore@" "${php_ini}"
 sed -i "s@^max_execution_time.*@max_execution_time = 300@" "${php_ini}"
@@ -459,23 +453,26 @@ sed -i "s@^expose_php.*@expose_php = Off@" "${php_ini}"
 sed -i "s@^short_open_tag.*@short_open_tag = On@" "${php_ini}"
 sed -i "s#mysqli.default_socket.*#mysqli.default_socket = ${sock_location}#" "${php_ini}"
 sed -i "s#pdo_mysql.default_socket.*#pdo_mysql.default_socket = ${sock_location}#" "${php_ini}"
-_error_detect "chown root:www-data /var/lib/php/{session,wsdlcache,opcache}"
+_error_detect "chown root:www-data /var/lib/php/{sessions,wsdlcache,opcache}"
 _info "PHP setup completed"
 
-_error_detect "cp -f ${cur_dir}/conf/lamp /usr/bin/"
-_error_detect "chmod 755 /usr/bin/lamp"
+_info "Addons installation start"
+_error_detect "apt-get install -y redis-server php${php_ver}-redis php${php_ver}-ds"
+[ -z "`grep ^maxmemory /etc/redis/redis.conf`" ] && sed -i "s@maxmemory <bytes>@maxmemory <bytes>\nmaxmemory 128000000@" /etc/redis/redis.conf
+_error_detect "systemctl restart redis-server"
+_error_detect "systemctl enable redis-server"
+_info "Addons setup completed"
+
+_error_detect "cp -f ${cur_dir}/conf/lnmp /usr/bin/"
+_error_detect "chmod 755 /usr/bin/lnmp"
 _error_detect "chown -R www-data:www-data /data/www /data/wwwlog"
-_error_detect "cp -f ${cur_dir}/conf/php.conf /etc/apache2/mods-available/"
-pushd /etc/apache2/mods-enabled/ >/dev/null 2>&1
-ln -s ../mods-available/php.conf php.conf
-popd >/dev/null 2>&1
 _error_detect "systemctl daemon-reload"
 _error_detect "systemctl start ${php_fpm}"
-_error_detect "systemctl start apache2"
+_error_detect "systemctl start nginx"
 sleep 3
 _error_detect "systemctl restart ${php_fpm}"
 sleep 1
-_error_detect "systemctl restart apache2"
+_error_detect "systemctl restart nginx"
 sleep 1
 if [ -f "/etc/mysql/debian.cnf" ]; then
     cp -p /etc/mysql/debian.cnf /etc/mysql/debian.cnf.bak
@@ -496,16 +493,16 @@ _info "systemctl enable mariadb"
 systemctl enable mariadb >/dev/null 2>&1
 _info "systemctl enable ${php_fpm}"
 systemctl enable "${php_fpm}" >/dev/null 2>&1
-_info "systemctl enable apache2"
-systemctl enable apache2 >/dev/null 2>&1
+_info "systemctl enable nginx"
+systemctl enable nginx >/dev/null 2>&1
 pkill -9 gpg-agent
 _info "systemctl status mariadb"
 systemctl --no-pager -l status mariadb
 _info "systemctl status ${php_fpm}"
 systemctl --no-pager -l status "${php_fpm}"
-_info "systemctl status apache2"
-systemctl --no-pager -l status apache2
+_info "systemctl status nginx"
+systemctl --no-pager -l status nginx
 echo
 netstat -nxtulpe
 echo
-_info "LAMP (Linux + Apache + MariaDB + PHP) installation completed"
+_info "LNMP (Linux + Nginx + MariaDB + PHP) installation completed"
